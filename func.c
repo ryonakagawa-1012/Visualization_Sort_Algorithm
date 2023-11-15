529aeb3f7058385c1edd6ce97f15b308811ce40e
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-11-15 09:13:11 +0900

    Initial commit

---
+++ func.c
@@ -0,0 +1,11 @@
+//
+// Created by 中川諒 on 2023/11/13.
+//
+#include <stdio.h>
+int start(void){
+    int algorithm;
+    printf("見たいソートアルゴリズムを選んでください\n");
+
+    scanf("%d", &algorithm);
+    return algorithm;
+}
---
+++ main.c
@@ -0,0 +1,15 @@
+//
+// Created by 中川諒 on 2023/11/13.
+//
+#include <stdio.h>
+#include <handy.h>
+
+int start(void);
+
+int main (){
+    int algorithm = start();
+
+    printf("%d", algorithm);
+
+    return algorithm;
+}
