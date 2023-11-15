9c30e66c9f865d0a9a11c561cfb1fe7f90a7d66c
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-11-15 10:09:48 +0900

    [add] コードの雛形完成

--- func.c
+++ func.c
@@ -2,10 +2,56 @@
 // Created by 中川諒 on 2023/11/13.
 //
 #include <stdio.h>
-int start(void){
-    int algorithm;
-    printf("見たいソートアルゴリズムを選んでください\n");
+#include <handy.h>
 
+int select_algorithm(void){
+    int algorithm;
+    printf("見たいソートアルゴリズムを選んでください\n\n");
+    printf("0: バブルソート\n");
+    printf("1: 選択ソート\n");
+    printf("2: 挿入ソート\n");
+    printf("3: クイックソート\n");
+    printf("4: マージソート\n");
+    printf("5: ヒープソート\n");
+    printf("6: ボゴソート\n\n");
+
+    printf(">>> ");
     scanf("%d", &algorithm);
     return algorithm;
+}
+
+int sort_length(void){
+    int length;
+    printf("ソートする配列の長さを決めてください\n\n");
+    printf(">>> ");
+    scanf("%d", &length);
+    return length;
+}
+
+void bubbleSort(int length){
+
+}
+
+void selectionSort(int length){
+
+}
+
+void insertionSort(int length){
+
+}
+
+void quickSort(int length){
+
+}
+
+void mergeSort(int length){
+
+}
+
+void heapSort(int length){
+
+}
+
+void bogoSort(int length){
+
 }
--- main.c
+++ main.c
@@ -4,12 +4,66 @@
 #include <stdio.h>
 #include <handy.h>
 
-int start(void);
+#define WINDOW_X 1000
+#define WINDOW_Y 800
+
+int select_algorithm(void); // 見たいアルゴリズムを選ぶCUI画面を表示する関数
+int sort_length(void); // ソートする配列の長さを決める関数
+
+void bubbleSort(int Length); // バブルソートを行う関数
+void selectionSort(int Length); // 選択ソートを行う関数
+void insertionSort(int Length); // 挿入ソートを行う関数
+void quickSort(int Length); // クイックソートを行う関数
+void mergeSort(int Length); // マージソートを行う関数
+void heapSort(int Length); // ヒープソートを行う関数
+void bogoSort(int Length); // ボゴソートを行う関数
+
 
 int main (){
-    int algorithm = start();
+    int algorithm = select_algorithm();
 
-    printf("%d", algorithm);
+    switch (algorithm) {
+        case 0:
+            printf("バブルソートを選択しました\n\n");
+            bubbleSort(sort_length());
+            break;
 
-    return algorithm;
+        case 1:
+            printf("選択ソートを選択しました\n\n");
+            selectionSort(sort_length());
+            break;
+
+        case 2:
+            printf("挿入ソートを選択しました\n\n");
+            insertionSort(sort_length());
+            break;
+
+        case 3:
+            printf("クイックソートを選択しました\n\n");
+            quickSort(sort_length());
+            break;
+
+        case 4:
+            printf("マージソートを選択しました\n\n");
+            mergeSort(sort_length());
+            break;
+
+        case 5:
+            printf("ヒープソートを選択しました\n\n");
+            heapSort(sort_length());
+            break;
+
+        case 6:
+            printf("ボゴソートを選択しました\n\n");
+            bogoSort(sort_length());
+            break;
+
+        default:
+            break;
+    }
+
+//    HgGetChar();
+//    HgClose();
+
+    return 0;
 }
