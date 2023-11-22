c65009d73641c90645787e6abf592cba36173855
Ryo <ryo.na.na.ryo.na@gmail.com>
2023-11-22 09:45:15 +0900

    [add] main.hの追加

---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by 中川諒 on 2023/11/22.
+//
+
+#ifndef SORT_ALGORITHM_MAIN_H
+#define SORT_ALGORITHM_MAIN_H
+
+#include <stdio.h>
+#include <handy.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define WINDOW_X 1000
+#define WINDOW_Y 600
+
+#endif //SORT_ALGORITHM_MAIN_H
